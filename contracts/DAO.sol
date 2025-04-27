3e658e26d89499bc397d30734e6bb19c06902286
Jeff Desalle <jeffdesalle@gmail.com>
2025-04-27 11:59:48 -0700

    wk4 Voting, investor only and tests

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -21,6 +21,8 @@ contract DAO {
     uint256 public proposalCount;
     mapping(uint256 => Proposal) public proposals;
 
+    mapping(uint256 => mapping(address => bool)) votes;
+
     event Propose(
         uint id,
         uint256 amount,
@@ -28,7 +30,10 @@ contract DAO {
         address creator
     ); 
 
-
+    event Vote(
+        uint256 id,
+        address investor
+    );
 
     constructor(Token _token, uint256 _quorum) {
         owner = msg.sender;
@@ -41,7 +46,7 @@ contract DAO {
 
     modifier onlyInvestor() {
         require(
-            Token(token).balanceOf(msg.sender) > 0,
+            token.balanceOf(msg.sender) > 0,
             "Must be token holder"
         );
         _;
@@ -52,7 +57,7 @@ contract DAO {
         uint256 _amount,
         address payable _recipient
     ) external onlyInvestor {
-        require(address(this).balance >= _amount, "Insufficient funds");
+        require(address(this).balance >= _amount);
 
         // Increment proposal count
         proposalCount++;
@@ -72,7 +77,24 @@ contract DAO {
             proposalCount,
             _amount,
             _recipient,
-            msg.sender);
+            msg.sender
+        );
     }   
+
+
+    function vote(uint256 _id) external onlyInvestor {
+
+        Proposal storage proposal = proposals[_id];
+
+        //require(!votes[msg.sender][_id], "Already voted");
+        require(!votes[_id][msg.sender], "Already voted");
+
+        proposal.votes += token.balanceOf(msg.sender);
+
+        //votes[msg.sender][_id] = true;
+        votes[_id][msg.sender] = true;
+
+        emit Vote(_id, msg.sender);
+    }
 
 }
